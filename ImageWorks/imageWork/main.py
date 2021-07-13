6be1ee33eb78d6b27b5bdadba8856ea4abefc144
Jacob Louthan <Jacob Louthan@DESKTOP-OOOAI3T>
2021-07-12 22:12:20 -0400

    Added some functions and code in the main to demo them

--- ImageWorks/imageWork/main.py
+++ ImageWorks/imageWork/main.py
@@ -3,6 +3,25 @@ Created on Jul 12, 2021
 
 @author: Jacob Louthan
 '''
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+
+from image_functions import *
 
 if __name__ == '__main__':
-    pass
+    # open an image file. The default path is where this python module is
+    #im = Image.open("SiriusAndViolet.jpg")
+    #print(im.width, im.height, im.mode, im.format) # Display some info about the image
+    
+    #my_image = load_image("SiriusAndViolet.jpg")
+    #if my_image != None:
+    #    my_image.show(my_image)
+        
+    #im = Image.open("SiriusAndViolet.jpg")
+    #im_c = im.crop((200,300,400,500)) # (left, top, right, bottom) it's a tuple!
+    #im_c.show()
+    
+    my_image = blur_image(load_image("SiriusAndViolet.jpg"))
+    my_image.show()
